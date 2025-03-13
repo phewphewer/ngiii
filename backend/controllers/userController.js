33980cbbe00e5fe41f1621f54be4bf66214c7f59
Josh <joshvvidal23@gmail.com>
2025-03-14 00:51:34 +0800

    npm install bcrypt
    Ep 3 Done
    npm install validator
    encryption of password and validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,18 +2,18 @@ const User = require("../models/userModel");
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({ mssg: "Log in User" });
+  res.json({ mssg: "Log in User" });
 };
 // create_account user
 const create_account_user = async function (req, res) {
-    const { email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        const user = await User.create_account(email, password);
-        res.status(200).json({ email, user });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const user = await User.create_account(username, email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { create_account_user, loginUser };
